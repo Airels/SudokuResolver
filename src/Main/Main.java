23e094dcc2b1f6c7430eca46cd2932b222b48334
Airels <yohan13240@hotmail.fr>
2019-10-17 20:19:09 +0200

    Added Purple ANSI code

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -14,7 +14,8 @@ public class Main {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_BLUE = "\u001b[36m";
+    public static final String ANSI_BLUE = "\u001B[36m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
 
     private int[] entries = {
             0, 6, 0,     0, 4, 0,    0, 0, 3,
