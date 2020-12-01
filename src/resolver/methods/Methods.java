497466cdfb70f358b1b96a332d1d9eb9c03b59ad
Airels <yohan13240@hotmail.fr>
2020-12-01 18:38:02 +0100

    Added enum of Methods

---
+++ src/resolver/methods/Methods.java
@@ -0,0 +1,18 @@
+package resolver.methods;
+
+public enum Methods {
+    UNIQUE_POSSIBILITY(new UniquePossibility()),
+    EXCLUSION(new Exclusion()),
+    EXCLUSIVE_PAIR_ONE_NUMBER(new ExclusivePairOneNumber()),
+    EXCLUSIVE_PAIR_TWO_NUMBERS(new ExclusivePairTwoNumbers());
+
+    private Method method;
+
+    Methods(Method method) {
+        this.method = method;
+    }
+
+    public Method getMethodInstance() {
+        return method;
+    }
+}
